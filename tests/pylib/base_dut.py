10a61437407e84f86874c66ba5e298b3b9053d3c
Ethan Tola <ethanltola@gmail.com>
2020-09-05 12:06:12 -0400

    Refactored and completed new functionality.
    It required a change in how the tests themselves are ran. The base dut
    now contains the methods to compile, upload, and verify the test
    configurations given to it.
    
    To support multiple duts, a special suffix is used (similar to tests) to
    match the _dut to the _tests suite. This way, its very easy to match the
    board with the test it should execute. Multiple dut configurations will
    run all the tests associated with them.
    
    Signed-off-by: Ethan Tola <ethanltola@gmail.com>

--- tests/pylib/base_dut.py
+++ tests/pylib/base_dut.py
@@ -1,3 +1,4 @@
+import subprocess
 from .base_test_config import BaseTestConfig
 
 class BaseDut(object):
@@ -16,6 +17,50 @@ class BaseDut(object):
         for key, value in zip(self.extras.keys(), self.extras.values()):
             base.append(key + "=" + value)
         return ":".join(base)
+    
+    def compile(self, test : BaseTestConfig):
+        print("Compiling {}...".format(test))
+        subprocess.check_call(["arduino-cli.exe", "compile", "--fqbn", self.fqbn, "--libraries", ".\\libraries", str(test)])
+
+    def upload(self, test : BaseTestConfig):
+        print("Uploading {}...".format(test))
+        subprocess.check_call(["arduino-cli.exe", "upload", "--fqbn", self.fqbn, "-p", self.port, str(test)])
+
+    def verify(self, test : BaseTestConfig):
+        import re
+        import time
+        import serial
+
+        print("Verifying {}...".format(test))
+        affirmative = ["SUCCESS", "success", "PASSED", "passed"]
+        negative = ["FAILURE", "failure", "FAILED", "failed"]
+        test_status_verifier = re.compile(r"(?P<status>{yes}|{no})".format(yes="|".join(affirmative), no="|".join(negative)))
+        ser = serial.Serial(
+            port = self.port,
+            baudrate = self.baud_rate,
+            parity = serial.PARITY_NONE,
+            stopbits = serial.STOPBITS_ONE,
+            bytesize = serial.EIGHTBITS,
+            timeout = 0
+        )
+        print("Connection established. Searching for test status code...")
+
+        timeout = 15
+        complete_str = ""
+        status = "UNKNOWN"
+        for _ in range(1, timeout):
+            if ser.in_waiting:
+                input_str = ser.read_all().decode('utf-8')
+                print(input_str, end="")
+                complete_str += input_str
+                match = test_status_verifier.search(complete_str)
+                if match is not None:
+                    status = "SUCCESS" if match.group('status') in affirmative else "FAILURE"
+                    break
+            time.sleep(1)
+        
+        ser.close()
+        print("Test {} status is was '{}'!".format(test, status))
 
     def __str__(self):
         return "\n".join([
--- tests/pylib/config_data.py
+++ tests/pylib/config_data.py
@@ -3,15 +3,19 @@ from .base_dut import BaseDut
 
 class ConfigData(object):
     def __init__(self, **fields):
-        self.dut = dict()
-        self.test_suite = ""
+        self.duts = dict()
         self.tests = []
         self.__dict__.update(fields)
+        # Create and add dut objects to dict
+        for dut_name in [dut_name for dut_name in fields.keys() if "_dut" in dut_name]:
+            self.duts[dut_name] = BaseDut(**fields[dut_name])
+        # Create test objects
         temp_tests = self.tests.copy()
         self.tests.clear()
-        for test in temp_tests:
-            self.tests.append(BaseTestConfig(**test))
-        self.dut = BaseDut(**self.dut)
+        for dut_name in self.duts.keys():
+            dut_name_trimmed = dut_name.replace("_dut", "")
+            for test in temp_tests:
+                self.tests.append((self.duts[dut_name], BaseTestConfig(dut_name_trimmed, **test)))
     
     @staticmethod
     def create_from_json(json_file : str):
@@ -20,9 +24,13 @@ class ConfigData(object):
             test_config = json.load(fp)
             return ConfigData(**test_config)
 
+    def compile_all(self):
+        for test in self.tests:
+            test[0].compile(test[1])
+            yield test
+
     def __str__(self):
         return "\n".join([
-            "Dut: {\n" + str(self.dut) + "\n}",
-            "Test Suite: " + self.test_suite,
+            "Duts: " + ",".join(["\n{\n" + "{}: {}".format(dut, str(self.duts[dut])) + "\n}" for dut in self.duts.keys()]),
             "Tests: " + ",".join([str(test) for test in self.tests])
         ])
