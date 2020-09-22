a8f429b299eb200618b4c52fd9cc663916604ba7
Ethan Tola <ethanltola@gmail.com>
2020-09-22 09:22:21 -0400

    Fixed homescreen issues.
    Transitions properly.
    
    Signed-off-by: Ethan Tola <ethanltola@gmail.com>

--- src/mega/home_screen.cpp
+++ src/mega/home_screen.cpp
@@ -11,79 +11,78 @@ uint8_t btn_callback_function_home(void* a, GuiElement* element, uint8_t event);
 
 HomeScreen::HomeScreen(int16_t _x, int16_t _y, int16_t _width, int16_t _height)
 {
-    x = _x;
-    y = _y;
-    width = _width;
-    height = _height;
-    
-    str_val_TS = new GuiLabel(240, 10, 60, 60, "TS 100%");
-    str_val_TS->textAlignH = TEXT_H_ALIGN_CENTRE;
-    str_val_TS->fontSize(3);
-    str_val_FS = new GuiLabel(380, 10, 60, 60, "FS 100%");
-    str_val_FS->textAlignH = TEXT_H_ALIGN_CENTRE;
-    str_val_FS->fontSize(3);
-    str_val_Percentage = new GuiLabel(240, 140, 60, 60, "50%");
-    str_val_Percentage->textAlignH = TEXT_H_ALIGN_CENTRE;
-    str_val_Percentage->fontSize(12);
-    str_val_Weight = new GuiLabel(120, 260, 60, 60, "Weight");
-    str_val_Weight->textAlignH = TEXT_H_ALIGN_CENTRE;
-    str_val_Weight->fontSize(4);
-    str_val_Tolerance = new GuiLabel(300, 260, 60, 60, "Toler");
-    str_val_Tolerance->textAlignH = TEXT_H_ALIGN_CENTRE;
-    str_val_Tolerance->fontSize(4);
-    buttonSettings = new GuiButton(20, 20, 120, 60, "Settings"); //(x,y,width,height,text)
+	x = _x;
+	y = _y;
+	width = _width;
+	height = _height;
+	
+	str_val_TS = new GuiLabel(240, 10, 60, 60, "TS 100%");
+	str_val_TS->textAlignH = TEXT_H_ALIGN_CENTRE;
+	str_val_TS->fontSize(3);
+	str_val_FS = new GuiLabel(380, 10, 60, 60, "FS 100%");
+	str_val_FS->textAlignH = TEXT_H_ALIGN_CENTRE;
+	str_val_FS->fontSize(3);
+	str_val_Percentage = new GuiLabel(240, 140, 60, 60, "50%");
+	str_val_Percentage->textAlignH = TEXT_H_ALIGN_CENTRE;
+	str_val_Percentage->fontSize(12);
+	str_val_Weight = new GuiLabel(120, 260, 60, 60, "Weight");
+	str_val_Weight->textAlignH = TEXT_H_ALIGN_CENTRE;
+	str_val_Weight->fontSize(4);
+	str_val_Tolerance = new GuiLabel(300, 260, 60, 60, "Toler");
+	str_val_Tolerance->textAlignH = TEXT_H_ALIGN_CENTRE;
+	str_val_Tolerance->fontSize(4);
+	buttonSettings = new GuiButton(20, 20, 120, 60, "Settings"); //(x,y,width,height,text)
 
 	buttonSettings->connectCallback(btn_callback_function_home, this);
 	
-    GuiElement::addChild((GuiElement *)str_val_TS);
-    GuiElement::addChild((GuiElement *)str_val_FS);
-    GuiElement::addChild((GuiElement *)str_val_Percentage);
-    GuiElement::addChild((GuiElement *)str_val_Weight);
-    GuiElement::addChild((GuiElement *)str_val_Tolerance);
-    GuiElement::addChild((GuiElement *)buttonSettings);
+	GuiElement::addChild((GuiElement *)str_val_TS);
+	GuiElement::addChild((GuiElement *)str_val_FS);
+	GuiElement::addChild((GuiElement *)str_val_Percentage);
+	GuiElement::addChild((GuiElement *)str_val_Weight);
+	GuiElement::addChild((GuiElement *)str_val_Tolerance);
+	GuiElement::addChild((GuiElement *)buttonSettings);
 
 }
 
 void HomeScreen::load(const void* params)
 {
-    
+	
 	if (params != NULL)
-    {
-        HomeLoadData* data = (HomeLoadData*)params;
-        strcpy(str_val_lbl_text1, (const char*)(data->weight));
-        str_val_Weight->text(str_val_lbl_text1);
-    }
-    else
-    {
-      strcpy(str_val_lbl_text1, "0");
-      str_val_Weight->text(str_val_lbl_text1);
-    }
+	{
+		HomeLoadData* data = (HomeLoadData*)params;
+		strcpy(str_val_lbl_text1, (const char*)(data->weight));
+		str_val_Weight->text(str_val_lbl_text1);
+	}
+	else
+	{
+	  strcpy(str_val_lbl_text1, "0");
+	  str_val_Weight->text(str_val_lbl_text1);
+	}
 }
 
-void* HomeScreen::unload()
+const void* HomeScreen::unload()
 {
-    return (void*)str_val_Weight->text();
+	return (void*)str_val_Weight->text();
 }
 
 void HomeScreen::update()
 {
-    return; // Nothing to do
+	return; // Nothing to do
 }
 
 // This will be called when the button is pressed or released
 uint8_t btn_callback_function_home(void* a, GuiElement* element, uint8_t event) {
-    if (event == GUI_EVENT_PRESS)
-    {
-
-    }
-    else if (event == GUI_EVENT_RELEASE)
-    {
+	if (event == GUI_EVENT_PRESS)
+	{
+		input1 = ((GuiButton*)element)->text();
+	}
+	else if (event == GUI_EVENT_RELEASE)
+	{
 		if (input1 == "Settings")
-	    {
-	    	Serial.print("Yo");
-	      screen_manager->switch_screen(SCREENS[0]);
-	    }
+		{
+			screen_manager->switch_screen(SCREENS[0]);
+		}
 
-    }
-    return 0;
+	}
+	return 0;
 }
