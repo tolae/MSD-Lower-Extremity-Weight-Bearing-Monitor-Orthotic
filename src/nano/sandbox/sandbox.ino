879140a5ae2ad3c271ca08b91204dd77761cd818
Ethan Tola <ext9285@g.rit.edu>
2020-12-15 22:19:56 -0500

    Transformed into AT command helper.
    Signed-off-by: Ethan Tola <ext9285@g.rit.edu>

--- src/nano/sandbox/sandbox.ino
+++ src/nano/sandbox/sandbox.ino
@@ -1,33 +1,29 @@
 #include <Arduino.h>
+#include <AltSoftSerial.h>
 
-uint8_t mux_sel;
-uint16_t sensor_input0;
-uint16_t sensor_input1;
+#define CONNECTED 7
+
+AltSoftSerial BTserial;
 
 void setup() {
     Serial.begin(9600);
     while(!Serial);
 
-    mux_sel = 0;
+    BTserial.begin(9600);
 
     delay(1000);
 }
 
 void loop() {
-    digitalWrite(2, mux_sel & 0x01);
-    digitalWrite(3, mux_sel & 0x02);
-
-    delay(1);
-    
-    sensor_input0 = analogRead(A7);
-    sensor_input1 = analogRead(A6);
-
-    Serial.print("Weight A: ");
-    Serial.println(sensor_input0);
-    Serial.print("Weight B: ");
-    Serial.println(sensor_input1);
-
-    mux_sel = (++mux_sel) % 4;
-
-    delay(1000);
+    if (Serial.available())
+    {
+        String str = Serial.readString();
+        Serial.println(str);
+        BTserial.print(str);
+    }
+    if (BTserial.available())
+    {
+        String str = BTserial.readString();
+        Serial.println(str);
+    }
 }
